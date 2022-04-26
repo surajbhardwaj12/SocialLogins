6caf5cd0f1fcc68c5ae8992aad0c988ccc132d3c
Avinashg137 <avinashg.itpath@gmail.com>
2022-04-26 10:48:23 -0700

    Done :- Add table view [X]

--- SocialLogins/ViewController.swift
+++ SocialLogins/ViewController.swift
@@ -7,10 +7,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+
+
+class ViewController: UIViewController,UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arr.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell",
+                                                     for: indexPath)
+            cell.textLabel?.text = self.arr[indexPath.row]
+            return cell
+        
+    }
+    
+    var arr = ["Google","Facebook","AppleId"]
+
+    @IBOutlet weak var UItableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        UItableView.dataSource = self
+        UItableView.delegate = self
         // Do any additional setup after loading the view.
     }
 
