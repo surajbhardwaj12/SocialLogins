b961eef0281e6ae67952a0c47c7bc7819e7076e9
surajbhardwaj12 <surajb.itpath@gmail.com>
2022-04-26 09:42:42 +0530

    Initial Commit

---
+++ SocialLogins/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SocialLogins
+//
+//  Created by MAC OS 17 on 26/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
